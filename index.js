e83230a5dcdef81c0cd47e78e4dca39a4f35311a
Mel <160260061+mel-muhina@users.noreply.github.com>
2024-07-23 09:51:45 +0100

    Added environment variables.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Initialisation of the server
+require('dotenv').config()
 const app = require('./app')
-const port = 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
